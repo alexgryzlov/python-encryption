f436801c6cebf6c240bcf5a68652d2c9f1c11f0e
alexgryzlov <43905250+alexgryzlov@users.noreply.github.com>
2019-04-02 18:45:15 +0300

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+ru_alphabet_lower = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+ru_alphabet_upper = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+alphabets = {ru_alphabet_upper, ru_alphabet_lower}
--- frequencies.py
+++ frequencies.py
@@ -1,11 +1,11 @@
 import collections
 import math
+from constants import ru_alphabet_lower as ru_alphabet
 
 
 def make_frequency_table(message):
     """"Create frequency table (character returns its frequency)"""
     total_chars = 0
-    ru_alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     frequency_table = collections.Counter()
     for char in message.lower():
         if char in ru_alphabet:
@@ -19,7 +19,6 @@ def make_frequency_table(message):
 def parse_frequency_table(path):
     """"Parse frequency table from a file"""
     frequency_table = {}
-    ru_alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     for char in ru_alphabet:
         frequency_table[char] = 0
     file = open(path, 'r')
@@ -32,7 +31,6 @@ def parse_frequency_table(path):
 def calculate_frequency_difference(table1, table2):
     """"Calculate difference between 2 table by summing all squared differences of all characters frequencies"""
     difference = 0
-    ru_alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
     for letter in ru_alphabet:
         difference += (table1[letter] - table2[letter]) ** 2
     return difference
@@ -41,7 +39,7 @@ def calculate_frequency_difference(table1, table2):
 def find_best_shift(frequency_table, decrypter, encrypted_message):
     best_value = math.inf
     best_shift = 0
-    for shift in range(33):  # 33 - length of Russian alphabet
+    for shift in range(len(ru_alphabet)):
         decrypted_message = decrypter(encrypted_message, shift)
         current_frequency_table = make_frequency_table(decrypted_message)
         current_value = calculate_frequency_difference(frequency_table, current_frequency_table)
